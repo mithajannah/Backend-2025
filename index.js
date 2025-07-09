40f2e07f105a93000674d6583f6dc30ef0b7dcde
mithajannah <mifthuljannah12@gmail.com>
2025-07-09 11:12:38 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
-const express = require ('express');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const mahasiswaRoutes = require('./routes/mahasiswa');
+const matakuliahRoutes = require('./routes/matakuliah');
+
 const app = express();
-const port = 3000;
-const users = require('./users')
-const mapel = require('./mapel')
-const siswa = require('./siswa')
-const guru = require('./guru')
-const kelas = require('./kelas')
-const jadwal = require('./jadwal')
-const nilai = require('./nilai')
-const presensi = require('./presensi')
+app.use(cors());
+app.use(express.json());
 
-//menggunakan modul
-app.use('/users', users);
-app.use('/mapel', mapel);
-app.use('/siswa', siswa);
-app.use('/guru', guru);
-app.use('/kelas', kelas);
-app.use('/jadwal', jadwal);
-app.use('/nilai', nilai);
-app.use('/presensi', presensi);
+app.use('/mahasiswa', mahasiswaRoutes);
+app.use('/matakuliah', matakuliahRoutes);
 
-//routing
-app.get("/", (req, res) =>{
-    res.send("Kirim data");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
 });
-
-app.post("/", (req, res) =>{
-    res.send("Kirim data");
-});
-
-app.put("/", (req, res) =>{
-    res.send("edit data");
-});
-
-app.patch("/", (req, res) =>{
-    res.send("edit data");
-});
-
-app.delete("/", (req, res) =>{
-    res.send("hapus data");
-});
-
-app.listen(port,()=>{
-    console.log(`Aplikasi berjalan pada : $(port)`);
-})
